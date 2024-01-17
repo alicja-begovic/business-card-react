892b57dd287d59c6daf9bab7a374a0a25fcee16b
Alicja Begović <alicja.begovic@gmail.com>
2024-01-17 23:36:28 +0100

    main shape and simple styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
-import "./App.css";
+import classes from './App.module.css';
+import myPhoto from "./assets/myPhoto.jpg";
+
 
 function App() {
   return (
     <>
-      <section>
-        <img src="" alt="My picture" />
-      </section>
-      <section>
-        <h2>Alicja Begović</h2>
-        <h3>junior frontend developer</h3>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus sunt
-          voluptatibus, nisi vitae dignissimos voluptates expedita fugiat.
-          Tempora facere aut autem dignissimos at dolorum culpa voluptate eaque,
-          placeat perspiciatis amet.
-        </p>
-        <p>My social media: LinkedIn, Facebook, github</p>
-      </section>
+      <div className={classes.card}>
+        <section>
+          <img className={classes.img} src={myPhoto} alt="My picture" />
+        </section>
+        <section>
+          <h2>Alicja Begović</h2>
+          <h3>junior frontend developer</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus sunt
+            voluptatibus, nisi vitae dignissimos voluptates expedita fugiat.
+            Tempora facere aut autem dignissimos at dolorum culpa voluptate
+            eaque, placeat perspiciatis amet.
+          </p>
+          <p>My social media: LinkedIn, Facebook, github</p>
+        </section>
+      </div>
     </>
   );
 }
